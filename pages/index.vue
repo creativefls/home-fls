452cb9bdeeffce51ac1a58f23b9602dd7bb04f91
wkwksama <mailpakgasing@gmail.com>
2018-03-26 16:47:35 +0700

    fix responsive insta feed

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,30 @@
 <template>
   <v-container fluid>
     <fls-quote></fls-quote>
-    <v-jumbotron color="success" dark>
-      <v-container fill-height>
-        <v-layout wrap>
-          <v-flex md6>
-            <v-card flat color="transparent">
+      <v-layout class="section-insta success" wrap>
+        <v-flex md6>
+          <v-card flat dark color="transparent">
+            <v-container dark>
               <h1 class="display-2">Keep in Touch</h1>
               <h1 class="headline">Official Line</h1>
               <img class="image line-barcode" src="https://user-images.githubusercontent.com/27270350/34720177-5c3e398c-f570-11e7-9aa4-2f340592da2c.jpg" alt="line barcode">
-            </v-card>
-          </v-flex>
-          <v-flex md6>
-            <v-card color="white" class="pa-2">
-              <v-container fluid grid-list-sm>
-                <v-layout row wrap>
-                  <v-flex xs4  v-for="(insta, index) in instaFeed" :key="insta.id" v-if="index < 9">
-                    <a href="https://www.instagram.com/flsummit/">
-                      <img class="image" :src="insta.node.thumbnail_src" alt="flsummit" width="100%" height="100%">
-                    </a>
-                  </v-flex>
-                </v-layout>
-              </v-container>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-jumbotron>
+            </v-container>
+          </v-card>
+        </v-flex>
+        <v-flex md6>
+          <v-card color="white" class="pa-2">
+            <v-container grid-list-sm>
+              <v-layout row wrap>
+                <v-flex xs4  v-for="(insta, index) in instaFeed" :key="insta.id" v-if="index < 9">
+                  <a href="https://www.instagram.com/flsummit/">
+                    <img class="image" :src="insta.node.thumbnail_src" alt="flsummit" width="100%" height="100%">
+                  </a>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card>
+        </v-flex>
+      </v-layout>
 
 
 
@@ -66,4 +64,6 @@ img.line-barcode {
   width auto
   height 256px
 }
+.section-insta
+  position relative
 </style>
