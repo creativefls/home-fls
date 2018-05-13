e4be2c454723f9379c3f2efccc9e87e9e5350e8d
wkwksama <mailpakgasing@gmail.com>
2018-05-14 00:42:33 +0700

    resize iframe google form roadshow surabaya

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,7 +1,7 @@
 <template>
   <v-layout>
     <v-flex text-xs-center>
-     <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSewdy7N9ymW7fZ0olIby1N7Mf4YurWZ2l2vuD6xIR7C1OQ8TQ/viewform?embedded=true" width="100%" height="1500" frameborder="0" marginheight="0" marginwidth="0">Memuat...</iframe>
+     <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSewdy7N9ymW7fZ0olIby1N7Mf4YurWZ2l2vuD6xIR7C1OQ8TQ/viewform?embedded=true" width="100%" height="2500" frameborder="0" marginheight="0" marginwidth="0">Memuat...</iframe>
     </v-flex>
   </v-layout>
 </template>
