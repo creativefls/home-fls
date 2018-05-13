12322229218e4bb57c3f19c15a4eebd9eec979a0
wkwksama <mailpakgasing@gmail.com>
2018-05-14 00:22:09 +0700

    roadshow surabaya

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,15 +1,7 @@
 <template>
   <v-layout>
     <v-flex text-xs-center>
-      <img src="/v.png" alt="Vuetify.js" class="mb-5">
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
+     <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSewdy7N9ymW7fZ0olIby1N7Mf4YurWZ2l2vuD6xIR7C1OQ8TQ/viewform?embedded=true" width="100%" height="1500" frameborder="0" marginheight="0" marginwidth="0">Memuat...</iframe>
     </v-flex>
   </v-layout>
 </template>
