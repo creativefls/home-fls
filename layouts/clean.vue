068e7a5786ae4386985d453cb126ca4167c4dd66
wkwksama <mailpakgasing@gmail.com>
2018-06-24 17:59:45 +0700

    halaman kirim pesan

---
+++ layouts/clean.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-app>
+    <v-toolbar :color="toolbarColor" class="d-flex" scroll-off-screen dense flat dark app>
+      <nuxt-link to="/">
+        <v-toolbar-title>
+          <v-avatar tile>
+            <img src="https://user-images.githubusercontent.com/21119252/34459239-16407fee-ee1d-11e7-94c1-dc6446f962b0.png" alt="FLS Logo">
+          </v-avatar>
+        </v-toolbar-title>
+      </nuxt-link>
+    </v-toolbar>
+    <v-content :class="$route.path == '/' ? 'pt-0' : 'padding-page'">
+      <nuxt />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      scroll: 0,
+    }
+  },
+  computed: {
+    toolbarColor () {
+      if (this.scroll < 50 && this.$route.path == '/') {
+        return 'transparent'
+      } else {
+        return 'accent transparent'
+      }
+    }
+  },
+  methods: {
+    onScroll (e) {
+      this.scroll = document.documentElement.scrollTop
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+nav
+  justify-content center
+  padding 6px 0
+  .toolbar__content
+    @media only screen and (min-device-width: 960px)
+      max-width 80%
+  .toolbar__title
+    margin-left 0
+    .avatar.avatar--tile
+      height 36px !important
+      width auto !important
+  .btn
+    text-transform capitalize
+    font-size 16px
+
+.jumbotron
+  min-height 100vh
+  .display-3
+    @media only screen and (max-device-width: 960px)
+      font-size 40px !important
+
+.accent.transparent
+  background-color #2c3e50 !important
+
+.padding-page
+  padding-top 60px !important
+</style>
