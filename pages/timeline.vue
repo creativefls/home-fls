001dd454c7f72270d44f7e11a32bbb48025459a6
hermawanln <hermawan.luthfi@gmail.com>
2018-07-01 00:04:46 +0700

    timeline sementara done

--- pages/timeline.vue
+++ pages/timeline.vue
@@ -1,50 +1,169 @@
 <template>
-<v-layout column>
-  <timeline>
-    <timeline-title>title</timeline-title>
-    <timeline-item bg-color="#9dd8e0">
-            <v-flex xs12 sm8 md6> 
-                <v-card>
-                <v-chip style="margin-top: -16px;" class="ml-4" label color="urban" text-color="white">
-                <div class="subheading">
-                    <v-icon medium class="white--text">event</v-icon>
-                    <b>FLS 2011</b>
-                </div>
-                </v-chip>
-                <v-card-text>
-                <v-container fluid grid-list-lg>
-                        <v-layout row>
-                            <v-flex xs7>
-                            <div>
-                                <div class="headline grey">11 Mei 2011</div>                        
-                                <div class="headline"><b>"We are not talking about change, We are making change."</b></div><br>
-                                <div>
-                                <b class="txt-urban">1</b> Hari 
-                                <b class="txt-urban">6</b> Isu 
-                                <b class="txt-urban">14</b> Pembicara
-                                </div>
-                                <div>
-                                <b class="txt-urban">224</b> Peserta 
-                                <b class="txt-urban">517</b> Pendaftar
-                                </div>                        
-                            </div>
-                            </v-flex>
-                            <v-flex xs5>
-                            <v-card-media
-                                src="https://futureleadersummit.org/2017/assets/img/kilas/2011.png"
-                                height="125px"
-                                contain
-                            ></v-card-media>
-                            </v-flex>
-                        </v-layout>
-                        </v-container>
-                </v-card-text>
-            </v-card>
-        </v-flex>
-    </timeline-item>
-    <timeline-item :hollow="true">item2</timeline-item>
-  </timeline>
-</v-layout>
+<v-container>
+  <v-layout column>
+    <timeline>
+      <timeline-title><div class="headline"><b>HARI PERTAMA</b></div>Sabtu, 8 September 2018</timeline-title>      
+      <timeline-item bg-color="#9dd8e0">
+              <v-flex xs12 sm8 md6> 
+                  <v-card>
+                  <v-chip style="margin-top: -16px;" class="ml-4" label color="urban" text-color="white">
+                  <div class="subheading">
+                      <v-icon medium class="white--text">event</v-icon>
+                      <b>Welcoming Session</b>
+                  </div>
+                  </v-chip>
+                  <v-card-text>
+                  <v-container fluid grid-list-lg>
+                          <v-layout row>
+                            <v-flex xs>
+                                  <div class="headline">
+                                    Merupakan rangkaian pembukaan acara Future Leader Summit 2018 yang berisi
+                                    penyambutan delegasi pemuda dan penjelasan mengenai isu yang diangkat dan
+                                    rangkaian acara Future Leader Summit 2018.
+                                  </div>
+                            </v-flex>                            
+                          </v-layout>
+                          </v-container>
+                  </v-card-text>
+              </v-card>
+          </v-flex>
+      </timeline-item>
+      <timeline-item bg-color="#9dd8e0">
+              <v-flex xs12 sm8 md6> 
+                  <v-card>
+                  <v-chip style="margin-top: -16px;" class="ml-4" label color="urban" text-color="white">
+                  <div class="subheading">
+                      <v-icon medium class="white--text">event</v-icon>
+                      <b>Ideation Lab</b>
+                  </div>
+                  </v-chip>
+                  <v-card-text>
+                  <v-container fluid grid-list-lg>
+                          <v-layout row>
+                            <v-flex xs>
+                                  <div class="headline">
+                                    Merupakan salah satu sesi dalam rangkaian acara FLS 2018 
+                                    yang akan membahas berbagai isu terkini yang berkaitan dengan 
+                                    6 room dengan bimbingan seorang pakardalam isu tersebut. 
+                                    Hasil akhir dari sesi ini adalah sebuah idea canvas yang akandisusun 
+                                    secara berkelompok dengan tujuan utama: Indonesia emas 2045.
+                                  </div>
+                            </v-flex>                            
+                          </v-layout>
+                          </v-container>
+                  </v-card-text>
+              </v-card>
+          </v-flex>
+      </timeline-item>
+      <timeline-item bg-color="#9dd8e0">
+              <v-flex xs12 sm8 md6> 
+                  <v-card>
+                  <v-chip style="margin-top: -16px;" class="ml-4" label color="urban" text-color="white">
+                  <div class="subheading">
+                      <v-icon medium class="white--text">event</v-icon>
+                      <b>Future Leader in Action</b>
+                  </div>
+                  </v-chip>
+                  <v-card-text>
+                  <v-container fluid grid-list-lg>
+                          <v-layout row>
+                            <v-flex xs>
+                                  <div class="headline">
+                                    Sebagai validasi ide dari Ideation Lab, sesi ini merupakan sebuah role-play
+                                    games dimana para Delegates akan mendapatkan kesempatan untuk berperan
+                                    sebagai lembaga pemerintah dalam menyelesaikan beberapa studi kasus untuk
+                                    mengatasi permasalahan masyarakat dan juga sebagai sebuah gambaran dalam
+                                    mewujudkanIndonesia yang lebih baik di tahun 2045.
+                                  </div>
+                            </v-flex>                            
+                          </v-layout>
+                          </v-container>
+                  </v-card-text>
+              </v-card>
+          </v-flex>
+      </timeline-item>
+      <timeline-item bg-color="#9dd8e0">
+              <v-flex xs12 sm8 md6> 
+                  <v-card>
+                  <v-chip style="margin-top: -16px;" class="ml-4" label color="urban" text-color="white">
+                  <div class="subheading">
+                      <v-icon medium class="white--text">event</v-icon>
+                      <b>Gala Future Leader</b>
+                  </div>
+                  </v-chip>
+                  <v-card-text>
+                  <v-container fluid grid-list-lg>
+                          <v-layout row>
+                            <v-flex xs>
+                                  <div class="headline">
+                                    Sebuah ajang networking night pada Future Leader Summit 2018 dimana pada
+                                    momen ini para delegates bisa saling memperkenalkan diri, berbagi cerita,
+                                    pengalaman, maupun untuk saling menginspirasi satu sama lain. Kegiatan ini
+                                    bertujuan untuk saling mengakrabkan sesama Delegates maupun dengan Alumni
+                                    dan Rangers FLS.
+                                  </div>
+                            </v-flex>                            
+                          </v-layout>
+                          </v-container>
+                  </v-card-text>
+              </v-card>
+          </v-flex>
+      </timeline-item>
+      <timeline-title><div class="headline"><b>HARI KEDUA</b></div>Minggu, 9 September 2018</timeline-title>
+      <timeline-item bg-color="#9dd8e0">
+              <v-flex xs12 sm8 md6> 
+                  <v-card>
+                  <v-chip style="margin-top: -16px;" class="ml-4" label color="urban" text-color="white">
+                  <div class="subheading">
+                      <v-icon medium class="white--text">event</v-icon>
+                      <b>Discovery Panel</b>
+                  </div>
+                  </v-chip>
+                  <v-card-text>
+                  <v-container fluid grid-list-lg>
+                          <v-layout row>
+                            <v-flex xs>
+                                  <div class="headline">
+                                    Discovery Panel merupakan sesi dalam Future Leader Summit dimana delegates
+                                    akan mempelajari millennials must-have-skills yang akan tersedia dalam 10 pilihan
+                                    skill yang dapat dipilih oleh Delegates secara digital di hari sebelumnya.
+                                  </div>
+                            </v-flex>                            
+                          </v-layout>
+                          </v-container>
+                  </v-card-text>
+              </v-card>
+          </v-flex>
+      </timeline-item>
+      <timeline-item bg-color="#9dd8e0">
+              <v-flex xs12 sm8 md6> 
+                  <v-card>
+                  <v-chip style="margin-top: -16px;" class="ml-4" label color="urban" text-color="white">
+                  <div class="subheading">
+                      <v-icon medium class="white--text">event</v-icon>
+                      <b>Grand Summit</b>
+                  </div>
+                  </v-chip>
+                  <v-card-text>
+                  <v-container fluid grid-list-lg>
+                          <v-layout row>
+                            <v-flex xs>
+                                  <div class="headline">
+                                    Merupakan acara puncak dari Future Leader Summit dengan menghadirkan
+                                    keynote speaker inspiratif untuk memaparkan poin-poin penting tema utama dari
+                                    FLS 2018: “Preparing the Future Leaders to be ready to Take Charge of the Way in the
+                                    Disruptive Era”
+                                  </div>
+                            </v-flex>                            
+                          </v-layout>
+                          </v-container>
+                  </v-card-text>
+              </v-card>
+          </v-flex>
+      </timeline-item>
+    </timeline>
+  </v-layout>
+</v-container>
 </template>
 
 <script>
