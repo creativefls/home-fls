e9e74bddb6ed5787b23c441f013fc40f5439a38c
wkwksama <mailpakgasing@gmail.com>
2018-06-30 16:28:12 +0700

    implement new design

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -5,11 +5,11 @@ Vue.use(Vuetify, {
   theme: {
     primary: '#8e44ad', // a color that is not in the material colors palette
     secondary: '#1abc9c',
-    accent: '#2c3e50',
-    error: '#e74c3c',
-    info: '#3498db',
-    success: '#2ecc71',
-    warning: '#f1c40f',
+    accent: '#f4f2e9',
+    error: '#ef5924',
+    info: '#3883c7',
+    success: '#18bc97',
+    warning: '#feb34d',
     cloud: '#ecf0f1',
     silver: '#bdc3c7',
     urban: '#27AE60',
