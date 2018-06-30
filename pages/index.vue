6796cc517711a22c0d67b3d54e2b5cd4aa57ff2b
wkwksama <mailpakgasing@gmail.com>
2018-06-30 22:36:09 +0700

    section room

--- pages/index.vue
+++ pages/index.vue
@@ -4,26 +4,19 @@
       <fls-hero></fls-hero>
       <fls-countdown></fls-countdown>
     </div>
+    <fls-rooms></fls-rooms>
     <article-post />
-    <v-layout class="section-insta success" wrap justify-center>
-      <v-flex align-content-center="" md6 class="pb-5">
-        <v-card flat dark color="transparent">
-          <h1 class="headline text-xs-center follow">Official Line</h1>
-          <img class="image line-barcode" src="https://user-images.githubusercontent.com/27270350/34720177-5c3e398c-f570-11e7-9aa4-2f340592da2c.jpg" alt="line barcode">
-        </v-card>
-      </v-flex>
-
-    </v-layout>
   </v-container>
 </template>
 
 <script>
 import FlsHero from '@/components/sections/Hero'
 import FlsCountdown from '@/components/sections/Countdown'
+import FlsRooms from '@/components/sections/Rooms'
 import ArticlePost from '@/components/sections/ArticlePost'
 
 export default {
-  components: { ArticlePost, FlsHero, FlsCountdown },
+  components: { ArticlePost, FlsHero, FlsCountdown, FlsRooms },
   data () {
     return {
     }
