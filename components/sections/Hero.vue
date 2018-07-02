77eb3a03165c099950e87f6e879ffbc9fd7ac177
wkwksama <mailpakgasing@gmail.com>
2018-07-02 23:11:06 +0700

    fix width hero

--- components/sections/Hero.vue
+++ components/sections/Hero.vue
@@ -4,13 +4,11 @@
       <h1 class="display-3">
         Future Leader Summit 2018
       </h1>
-      <h2 class="subtitle mb-3">
-        <small>
-          Konferensi kepemudaan berskala nasional sebagai platform  kolaborasi yang digagas oleh Nusantara Muda.
-          <br>
-          FLS bertujuan untuk mempersiapkan pemimpin-pemimpin muda dari seluruh Indonesia dalam membuat perubahan signifikan untuk Indonesia yang lebih baik.
-        </small>
-      </h2>
+      <div class="subtitle mb-4 py-4">
+        Konferensi kepemudaan berskala nasional sebagai platform  kolaborasi yang digagas oleh Nusantara Muda.
+        <br>
+        FLS bertujuan untuk mempersiapkan pemimpin-pemimpin muda dari seluruh Indonesia dalam membuat perubahan signifikan untuk Indonesia yang lebih baik.
+      </div>
       <v-btn href="/daftar" color="info" large round depressed>Daftar Sekarang</v-btn>
     </v-flex>
   </v-layout>
