b7560de824d934cf39d6f95626abbe4d860b5b92
wkwksama <mailpakgasing@gmail.com>
2018-07-03 20:52:06 +0700

    halaman grogram add style timeline title

--- pages/program.vue
+++ pages/program.vue
@@ -5,13 +5,13 @@
     <v-flex md6>
       <v-layout>
         <timeline>
-          <timeline-title><div class="headline"><b>HARI PERTAMA</b></div>Sabtu, 8 September 2018</timeline-title>
-          <timeline-item bg-color="#9dd8e0">
+          <timeline-title class="pertama mb-4"><div class="headline"><b>HARI PERTAMA</b></div>Sabtu, 8 September 2018</timeline-title>
+          <timeline-item bg-color="#9dd8e0cc">
                   <v-flex xs12 >
                       <v-card>
-                      <v-chip style="margin-top: -16px;" class="ml-4" label color="urban" text-color="white">
-                      <div class="subheading">
-                          <v-icon medium class="white--text">event</v-icon>
+                      <v-chip style="margin-top: -16px;" class="ml-4 py-1 px-3" label color="urban" text-color="white">
+                      <div class="title">
+                          <v-icon left medium class="white--text">event</v-icon>
                           <b>Welcoming Session</b>
                       </div>
                       </v-chip>
@@ -19,7 +19,7 @@
                       <v-container fluid grid-list-lg>
                               <v-layout row>
                                 <v-flex xs>
-                                      <div class="headline">
+                                      <div class="program-content">
                                         Merupakan rangkaian pembukaan acara Future Leader Summit 2018 yang berisi
                                         penyambutan delegasi pemuda dan penjelasan mengenai isu yang diangkat dan
                                         rangkaian acara Future Leader Summit 2018.
@@ -31,12 +31,12 @@
                   </v-card>
               </v-flex>
           </timeline-item>
-          <timeline-item bg-color="#9dd8e0">
+          <timeline-item bg-color="#9dd8e0cc">
                   <v-flex xs12 >
                       <v-card>
-                      <v-chip style="margin-top: -16px;" class="ml-4" label color="urban" text-color="white">
-                      <div class="subheading">
-                          <v-icon medium class="white--text">event</v-icon>
+                      <v-chip style="margin-top: -16px;" class="ml-4 py-1 px-3" label color="urban" text-color="white">
+                      <div class="title">
+                          <v-icon left medium class="white--text">event</v-icon>
                           <b>Ideation Lab</b>
                       </div>
                       </v-chip>
@@ -44,7 +44,7 @@
                       <v-container fluid grid-list-lg>
                               <v-layout row>
                                 <v-flex xs>
-                                      <div class="headline">
+                                      <div class="program-content">
                                         Merupakan salah satu sesi dalam rangkaian acara FLS 2018
                                         yang akan membahas berbagai isu terkini yang berkaitan dengan
                                         6 room dengan bimbingan seorang pakardalam isu tersebut.
@@ -58,12 +58,12 @@
                   </v-card>
               </v-flex>
           </timeline-item>
-          <timeline-item bg-color="#9dd8e0">
+          <timeline-item bg-color="#9dd8e0cc">
                   <v-flex xs12 >
                       <v-card>
-                      <v-chip style="margin-top: -16px;" class="ml-4" label color="urban" text-color="white">
-                      <div class="subheading">
-                          <v-icon medium class="white--text">event</v-icon>
+                      <v-chip style="margin-top: -16px;" class="ml-4 py-1 px-3" label color="urban" text-color="white">
+                      <div class="title">
+                          <v-icon left medium class="white--text">event</v-icon>
                           <b>Future Leader in Action</b>
                       </div>
                       </v-chip>
@@ -71,7 +71,7 @@
                       <v-container fluid grid-list-lg>
                               <v-layout row>
                                 <v-flex xs>
-                                      <div class="headline">
+                                      <div class="program-content">
                                         Sebagai validasi ide dari Ideation Lab, sesi ini merupakan sebuah role-play
                                         games dimana para Delegates akan mendapatkan kesempatan untuk berperan
                                         sebagai lembaga pemerintah dalam menyelesaikan beberapa studi kasus untuk
@@ -85,12 +85,12 @@
                   </v-card>
               </v-flex>
           </timeline-item>
-          <timeline-item bg-color="#9dd8e0">
+          <timeline-item bg-color="#9dd8e0cc">
                   <v-flex xs12 >
                       <v-card>
-                      <v-chip style="margin-top: -16px;" class="ml-4" label color="urban" text-color="white">
-                      <div class="subheading">
-                          <v-icon medium class="white--text">event</v-icon>
+                      <v-chip style="margin-top: -16px;" class="ml-4 py-1 px-3" label color="urban" text-color="white">
+                      <div class="title">
+                          <v-icon left medium class="white--text">event</v-icon>
                           <b>Gala Future Leader</b>
                       </div>
                       </v-chip>
@@ -98,7 +98,7 @@
                       <v-container fluid grid-list-lg>
                               <v-layout row>
                                 <v-flex xs>
-                                      <div class="headline">
+                                      <div class="program-content">
                                         Sebuah ajang networking night pada Future Leader Summit 2018 dimana pada
                                         momen ini para delegates bisa saling memperkenalkan diri, berbagi cerita,
                                         pengalaman, maupun untuk saling menginspirasi satu sama lain. Kegiatan ini
@@ -116,18 +116,18 @@
       </v-layout>
     </v-flex>
   </v-layout>
-  <img src="/images/background-tangan.png" class="img-tangan" width="80%" alt="" style="margin-bottom: -40px; margin-left: 120px">  
-  <v-layout justify-center class="bg-success">
+  <img src="/images/background-tangan.png" class="img-tangan" width="80%" alt="" style="margin-bottom: -40px; margin-left: 120px">
+  <v-layout justify-center class="bg-info">
     <v-flex md6>
       <v-layout>
         <timeline>
-          <timeline-title><div class="headline"><b>HARI KEDUA</b></div>Minggu, 9 September 2018</timeline-title>
-          <timeline-item bg-color="#9dd8e0">
+          <timeline-title class="kedua mb-4"><div class="headline"><b>HARI KEDUA</b></div>Minggu, 9 September 2018</timeline-title>
+          <timeline-item bg-color="#9dd8e0cc">
                   <v-flex xs12 >
                       <v-card>
-                      <v-chip style="margin-top: -16px;" class="ml-4" label color="urban" text-color="white">
-                      <div class="subheading">
-                          <v-icon medium class="white--text">event</v-icon>
+                      <v-chip style="margin-top: -16px;" class="ml-4 py-1 px-3" label color="urban" text-color="white">
+                      <div class="title">
+                          <v-icon left medium class="white--text">event</v-icon>
                           <b>Discovery Panel</b>
                       </div>
                       </v-chip>
@@ -135,7 +135,7 @@
                       <v-container fluid grid-list-lg>
                               <v-layout row>
                                 <v-flex xs>
-                                      <div class="headline">
+                                      <div class="program-content">
                                         Discovery Panel merupakan sesi dalam Future Leader Summit dimana delegates
                                         akan mempelajari millennials must-have-skills yang akan tersedia dalam 10 pilihan
                                         skill yang dapat dipilih oleh Delegates secara digital di hari sebelumnya.
@@ -147,12 +147,12 @@
                   </v-card>
               </v-flex>
           </timeline-item>
-          <timeline-item bg-color="#9dd8e0">
+          <timeline-item bg-color="#9dd8e0cc">
                   <v-flex xs12 >
                       <v-card>
-                      <v-chip style="margin-top: -16px;" class="ml-4" label color="urban" text-color="white">
-                      <div class="subheading">
-                          <v-icon medium class="white--text">event</v-icon>
+                      <v-chip style="margin-top: -16px;" class="ml-4 py-1 px-3" label color="urban" text-color="white">
+                      <div class="title">
+                          <v-icon left medium class="white--text">event</v-icon>
                           <b>Grand Summit</b>
                       </div>
                       </v-chip>
@@ -160,7 +160,7 @@
                       <v-container fluid grid-list-lg>
                               <v-layout row>
                                 <v-flex xs>
-                                      <div class="headline">
+                                      <div class="program-content">
                                         Merupakan acara puncak dari Future Leader Summit dengan menghadirkan
                                         keynote speaker inspiratif untuk memaparkan poin-poin penting tema utama dari
                                         FLS 2018: “Preparing the Future Leaders to be ready to Take Charge of the Way in the
@@ -191,7 +191,7 @@ export default {
     TimelineTitle
   }
 }
-</script> 
+</script>
 
 <style lang="stylus">
 .card
@@ -212,4 +212,18 @@ export default {
   position absolute
   top: 0
   z-index -1
+
+.timeline-title
+  color #fff !important
+  border 2px solid white
+  border-radius 16px
+  padding 6px 18px
+  &.pertama
+    background-color #3883c7
+  &.kedua
+    background-color #feb34d
+
+.program-content
+  font-size 18px
+  font-weight 500
 </style>
