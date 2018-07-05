6f25f37fa26473b84d19c6be622331f692e8e586
hermawanln <hermawan.luthfi@gmail.com>
2018-07-05 11:52:45 +0700

    add link ref ke /daftar apabila tidak langsung redirect

--- pages/daftar.vue
+++ pages/daftar.vue
@@ -1,13 +1,14 @@
 <template>
   <v-layout>
-    <a href="http://daftar.futureleadersummit.org">
-      Redirecting...
-    </a>
+    <p style="margin-top:10px;margin-left:5px;height: 80vh;">
+      Redirecting... <br>
+      <a href="http://daftar.futureleadersummit.org">Klik disini jika halaman tidak berpindah</a>
+    </p>
   </v-layout>
 </template>
 
 <script>
 export default {
-  middleware: ['daftar']
+  // middleware: ['daftar']
 }
 </script>
