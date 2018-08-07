4e1fafd5dd0161d9ac914483e84e77c43d20f5cf
hermawanln <hermawan.luthfi@gmail.com>
2018-08-07 14:19:26 +0700

    lib sweetallert2, consume API

--- pages/pengumuman.vue
+++ pages/pengumuman.vue
@@ -25,6 +25,8 @@
                           <br class="my-2">
 
                           <vue-recaptcha
+                            v-validate="'required'"
+                            data-vv-as="captcha"
                             ref="recaptcha"
                             @verify="onVerify"
                             @expired="onExpired"
@@ -32,8 +34,8 @@
                           </vue-recaptcha>
                           <center>
                             <br class="my-1">
-                          <v-btn @click="submit">submit</v-btn>
-                          <v-btn @click="clear">clear</v-btn>
+                          <v-btn @click="submit" round>submit</v-btn>
+                          <v-btn @click="clear" round>clear</v-btn>
                           </center>
                       </form>
                     </v-layout>
@@ -53,6 +55,7 @@
 import Vue from 'vue'
 import VeeValidate from 'vee-validate'
 import VueRecaptcha from 'vue-recaptcha'
+import swal from 'sweetalert2'
 
   Vue.use(VeeValidate)
   Vue.use(VueRecaptcha)
@@ -85,8 +88,41 @@ import VueRecaptcha from 'vue-recaptcha'
 
     methods: {
       submit () {
-        this.$validator.validateAll()
-      },
+      this.$validator.validateAll().then((result) => {
+        if(!result) {
+          swal(
+            'Error',
+            'Sepertinya input yang kamu masukkan ada kekurangan. Silakan dicek kembali ya',
+            'error'
+          )
+        } else {
+          this.$axios.get('http://128.199.72.101:3000/explorer/', {
+            email: this.guestEmail
+          }).then(response => {
+            swal({
+              title: '<h6>WELCOME TO FUTURE LEADER SUMMIT 2018!</h6>',
+              width: 650,
+              imageUrl: 'https://user-images.githubusercontent.com/21119252/34459239-16407fee-ee1d-11e7-94c1-dc6446f962b0.png',
+              imageWidth: 100,
+              html: 
+                '<strong>Dear, <strong>Nama</strong> <strong>Profinsi</strong> !</strong>' +
+                '<p>Setelah melewati tahap seleksi, kami mengucapkan selamat karena kamu telah terpilih menjadi <br> Delegates Future Leader Summit 2018 di Room (Education)</p>' +
+                '<p>Mari mulai ambil bagian untuk perubahan dunia bersama 239 pemuda-pemudi terbaik Indonesia di konferensi Future Leader Summit yang diselenggarakan di Semarang tanggal 8-9 September 2018 mendatang. </p>'+
+                '<p>Silahkan cek emailmu untuk informasi selanjutnya!</p>' +
+                '<p>Terima Kasih </p>'
+            })
+            console.log('sukses, ', response)
+          }).catch(error => {
+            swal(
+              'Error',
+              'Sistem error',
+              'error'
+            )
+            console.log('error, ', error)
+          })
+        }
+      })
+    },
       clear () {
         this.email = ''
         this.$validator.reset()
