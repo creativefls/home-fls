4e6d4c82cb14c29d4bc4bdcc7d80d6a62578b7d3
hermawanln <hermawan.luthfi@gmail.com>
2018-09-01 16:34:42 +0700

    pembicara tanpa data array

--- pages/pembicara.vue
+++ pages/pembicara.vue
@@ -5,72 +5,173 @@
     <v-layout class="fullheight" align-center justify-center>
         <v-flex md10 text-xs-center>
             <div class="subtitle mb-4 py-4 black--text">
-                Rhenald Kasali merupakan seorang Guru Besar Ilmu Manajemen di Universitas Indonesia dan 
-                juga dikenal sebagai seorang praktisi bisnis yang sangat ulung dan telah menghasilkan 
-                banyak sekali tulisan yang menginspirasi banyak orang. Selain itu, beliau juga mendirikan 
-                Rumah Perubahan, sebuah yayasan yang bergerak dalam bidang pendidikan, Kesejahteraan sosial, 
-                danlingkungan hidup.
+                Faye Simanjuntak menjadi sosok yang inspiratif, saat usianya masih 12 tahun, ia 
+                mendirikan Rumah Faye, yaitu organisasi nirlaba yang fokus pada isu perdagangan anak.
+                 Kini, di usianya yang sudah menginjak 16 tahun ia semakin membuktikan bahwa usia muda 
+                 bukan menjadi penghalang untuk menjadi agen perubahan.
                 <br>
             </div>
             <div class="black--text">
                 <h1 class="display-2">
-                    RHENALD KASALI
+                    FAYE SIMANJUNTAK                    
                 </h1>
-                <img src="/images/pembicara1.png">
+                <img class="pembicara" src="/images/ga_pembicara1.png">
                 <h3 class="display-1">
-                    Praktisi Bisnis; <br>
-                    Founder Rumah Perubahan
+                    Co-Founder Rumah Faye                    
                 </h3>
             </div>
         </v-flex>
     </v-layout>
-    <img src="/images/background-tangan.png" class="img-tangan" width="80%" alt="" style="margin-bottom: -40px; margin-left: 120px">
+    <!-- <img src="/images/background-tangan.png" class="img-tangan" width="80%" alt="" style="margin-bottom: -40px; margin-left: 120px"> -->
     <v-layout justify-center class="bg-success white--text">
         <v-flex md10 text-xs-center>
             <br class="my-2">
             <div class="subtitle mb-4 py-4 black--text">
-                Alamanda Shantika pertama naik melalui karir cemerlangnya di salah satu startup 
-                bisnis transportasi di Indonesia sebagai Vice President of Product. Perjalannya berlanjut 
-                ketika beliau memutuskan untuk berpindah haluan untuk ikut berpartisipasi dalam membantu 
-                para startup muda Indonesia dalam mengembangkan bisnisnya melalui Binar Academy.
+                Iman Sjafei adalah salah pelaku industri kreatif di Indonesia, pernah berkarya 
+                menjadi seorang jurnalis, kini Ia melebarkan sayap menjadi CEO dari <i>Asumsi.co</i> sebuah 
+                platform media online serta menjadi CFO dari <i>Rekreasi.co</i> yang merupakan <i>creative 
+                agency nasional</i>.
                 <br>
             </div>
-            <div class="white--text">
+            <div class="black--text">
                 <h1 class="display-2">
-                    ALAMANDA SHANTIKA
+                    IMAN SJAFEI                    
                 </h1>
-                <img src="/images/pembicara2.png">
+                <img class="pembicara" src="/images/ga_pembicara2.png">
                 <h1 class="display-1">
-                    Founder Binar Academy
+                    CEO of <i>Asumsi.co</i> dan CFO of <i>Rekreasi.co</i>                    
                 </h1>
             </div>
             <img src="/images/background-tangan.png" class="img-tangan" width="80%" alt="" style="margin-bottom: -40px; margin-left: 120px">            
         </v-flex>
-    </v-layout>    
-    <v-layout justify-center class="bg-pembicara3 white--text">
+    </v-layout>
+
+    <!-- ROOM 1 -->
+    <v-layout justify-center class="bg-room1 white--text">
         <v-flex md10 text-xs-center>
             <br class="my-2">
-            <div class="subtitle mb-4 py-4 black--text">
-                Christian Rijanto merupakan seorang pengusaha muda yang sukses dalam grup bisnis 
-                miliknya yang bernama ISMAYA GROUP. Peursahaan yang menjalankan usahanya dalam bidang 
-                foodand beverage, Clubs, Property and hotel, Event organizer dan juga Music Publishing. 
-                Di usiayang masih tergolong produktif ini, beliau telah mencetak banyak prestasi seperti 
-                Indonesia'sTop 50 Creative Entrepreneur, Featured on BBC World as Indonesia's Rising 
-                Entrepreneur, FortuneMagazine "Top 40 Under 40" dan lain sebagainya.
+            <div class="display-2 mb-4 py-4 white--text">
+                ROOM HUMAN CAPITAL
                 <br>
             </div>
-            <div class="white--text">
+            <div class="black--text" xs6>
                 <h1 class="display-2">
-                    CHRISTIAN RIJANTO
+                    AGUNG BENZHARIE
                 </h1>
-                <img src="/images/pembicara3.png">
+                <img class="pembicara" src="/images/hc_pembicara1.png">
                 <h1 class="display-1">
-                    Managing Partner & Founder Ismaya Group
+                    CEO & Co-Founder Warung Pintar
                 </h1>    
             </div>
-            <br class="my-2">        
+            <img src="/images/background-tangan.png" class="img-tangan" width="80%" alt="" style="margin-bottom: -40px; margin-left: 120px">            
         </v-flex>
     </v-layout>
+    <!-- ROOM 2 -->
+    <v-layout justify-center class="bg-room2 white--text">
+        <v-flex md10 text-xs-center>
+            <br class="my-2">
+            <div class="display-2 mb-4 py-4 white--text">
+                ROOM POVERTY
+                <br>
+            </div>
+            <div class="black--text" xs6>
+                <h1 class="display-2">
+                    YOHANES SUGIHTONONUGROHO
+                </h1>
+                <img class="pembicara" src="/images/pov_pembicara1.png">
+                <h1 class="display-1">
+                    CEO & Co-Founder Crowde
+                </h1>    
+            </div>
+            <img src="/images/background-tangan.png" class="img-tangan" width="80%" alt="" style="margin-bottom: -40px; margin-left: 120px">            
+        </v-flex>
+    </v-layout>
+
+    <!-- ROOM 3 -->
+    <v-layout justify-center class="bg-room3 white--text">
+        <v-flex md10 text-xs-center>
+            <br class="my-2">
+            <div class="display-2 mb-4 py-4 white--text">
+                ROOM URBAN PLANING
+                <br>
+            </div>
+            <div class="black--text" xs6>
+                <h1 class="display-2">
+                    MARCO WIJAYAKUSUMA
+                </h1>
+                <img class="pembicara" src="/images/up_pembicara1.png">
+                <h1 class="display-1">
+                    Founder dan Direktur RUJAK Center For Urban Studies
+                </h1>    
+            </div>
+            <img src="/images/background-tangan.png" class="img-tangan" width="80%" alt="" style="margin-bottom: -40px; margin-left: 120px">            
+        </v-flex>
+    </v-layout>
+
+    <!-- ROOM 4 -->
+    <v-layout justify-center class="bg-room4 white--text">
+        <v-flex md10 text-xs-center>
+            <br class="my-2">
+            <div class="display-2 mb-4 py-4 white--text">
+                ROOM EDUCATION
+                <br>
+            </div>
+            <div class="black--text" xs6>
+                <h1 class="display-2">
+                    WINASTWAN GORA SWAJATI
+                </h1>
+                <img class="pembicara" src="/images/edu_pembicara1.png">
+                <h1 class="display-1">
+                    Founder <i>Kelase.com</i>
+                </h1>    
+            </div>
+            <img src="/images/background-tangan.png" class="img-tangan" width="80%" alt="" style="margin-bottom: -40px; margin-left: 120px">            
+        </v-flex>
+    </v-layout>
+
+    <!-- ROOM 5 -->
+    <v-layout justify-center class="bg-room5 white--text">
+        <v-flex md10 text-xs-center>
+            <br class="my-2">
+            <div class="display-2 mb-4 py-4 white--text">
+                ROOM DIGITAL
+                <br>
+            </div>
+            <div class="black--text" xs6>
+                <h1 class="display-2">
+                    TEGUH HARMANDA
+                </h1>
+                <img class="pembicara" src="/images/dig_pembicara1.png">
+                <h1 class="display-1">
+                    COO Tokocrypto
+                </h1>    
+            </div>
+            <img src="/images/background-tangan.png" class="img-tangan" width="80%" alt="" style="margin-bottom: -40px; margin-left: 120px">            
+        </v-flex>
+    </v-layout>
+    
+    <!-- ROOM 6 -->
+    <v-layout justify-center class="bg-room6 white--text">
+        <v-flex md10 text-xs-center>
+            <br class="my-2">
+            <div class="display-2 mb-4 py-4 white--text">
+                ROOM ENTERPRENEURSHIP
+                <br>
+            </div>
+            <div class="black--text" xs6>
+                <h1 class="display-2">
+                    RANI SOEBIJANTORO
+                </h1>
+                <img class="pembicara" src="/images/ent_pembicara1.png">
+                <h1 class="display-1">
+                    Co-Founder Bookabuku
+                </h1>    
+            </div>
+            <br class="my-2">
+            <!-- <img src="/images/background-tangan.png" class="img-tangan" width="80%" alt="" style="margin-bottom: -40px; margin-left: 120px">             -->
+        </v-flex>
+    </v-layout>
+
   </v-container>
 </template>
 
@@ -81,6 +182,44 @@ export default {
   components: { FlsHero },
   data () {
     return {
+        pembicara_rooms: [
+            {
+                title: 'ROOM HUMAN CAPITAL',
+                image: '/images/hc_pembicara1.png',
+                name: 'AGUNG BENZHARIE',
+                description: 'CEO & Co-Founder Warung Pintar.'
+            },
+            {
+                title: 'ROOM POVERTY',
+                image: '/images/pov_pembicara1.png',
+                name: 'YOHANES SUGIHTONONUGROHO',
+                description: 'CEO & Co-Founder Crowde.'
+            },
+            {
+                title: 'ROOM HUMAN CAPITAL',
+                image: '/images/hc_pembicara1.png',
+                name: 'AGUNG BENZHARIE',
+                description: 'CEO & Co-Founder Warung Pintar.'
+            },
+            {
+                title: 'ROOM HUMAN CAPITAL',
+                image: '/images/hc_pembicara1.png',
+                name: 'AGUNG BENZHARIE',
+                description: 'CEO & Co-Founder Warung Pintar.'
+            },
+            {
+                title: 'ROOM HUMAN CAPITAL',
+                image: '/images/hc_pembicara1.png',
+                name: 'AGUNG BENZHARIE',
+                description: 'CEO & Co-Founder Warung Pintar.'
+            },
+            {
+                title: 'ROOM HUMAN CAPITAL',
+                image: '/images/hc_pembicara1.png',
+                name: 'AGUNG BENZHARIE',
+                description: 'CEO & Co-Founder Warung Pintar.'
+            },
+        ]
     }
   },
   computed: {
@@ -95,6 +234,12 @@ img.line-barcode {
   width auto
   height 256px
 }
+
+img.pembicara {
+  height 256px
+  border-radius 50%
+  border solid 5px black 
+}
 .section-insta
   position relative
   background: #2ecc71;  /* fallback for old browsers */
@@ -107,8 +252,23 @@ img.line-barcode {
   display block
   margin 0 auto
 
-.bg-pembicara3
+.bg-room1
   background-color: #3883c7
+
+.bg-room2
+  background-color #FFA726
+
+.bg-room3
+  background-color #6D4C41
+
+.bg-room4
+  background-color #FF7043
+
+.bg-room5
+  background-color #FDD835
+
+.bg-room6
+  background-color #69F0AE
 
 .bg-hero
   // background: url('/images/background-unyu.png') top center no-repeat
