0a8643e5f5f97c4c865bfd884aa445c84ddb9492
haqqer <ilhamserbaguna@gmail.com>
2019-08-01 15:06:59 +0700

    update add cek pendaftaran

--- components/sections/RegisterArea.vue
+++ components/sections/RegisterArea.vue
@@ -2,7 +2,14 @@
   <v-layout class="fullheight" align-center justify-center>
     <v-flex md5 text-xs-center class="white--text">        
       <h1 class="display-3 my-5 py-5">Register Your Self</h1>
-      <v-btn color="error" round depressed class="px-5" href="http://join.futureleadersummit.org"><h1>REGISTER HERE</h1></v-btn>
+      <v-layout justify-center row wrap>
+        <v-flex xs12 md6>
+          <v-btn color="error" round depressed class="px-5" href="http://join.futureleadersummit.org"><h2>REGISTER HERE</h2></v-btn>
+        </v-flex>
+        <v-flex xs12 md6>
+          <v-btn color="error" round depressed class="px-5" href="http://join.futureleadersummit.org/resend"><h2>Cek Pendaftaran</h2></v-btn>          
+        </v-flex>
+      </v-layout>
         <h1 class="display-2 my-5 py-5">BECOME SPONSOR</h1>
       <div class="headline mb-4 py-4">
         <v-btn color="error" round depressed>CONTACT US</v-btn>
